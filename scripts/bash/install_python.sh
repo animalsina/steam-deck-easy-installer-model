0e66fc685f9f15a5916b201349d02ebbde545e04
AnimaLSina <peppenps@gmail.com>
2024-08-13 17:24:10 +0200

    Update install_python file

--- scripts/bash/install_python.sh
+++ scripts/bash/install_python.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-install_python() {
+install_python_and_pipx() {
     if ! command -v python3 &>/dev/null; then
         echo "Python3 not found, installing..."
-        sudo apt update
-        sudo apt install -y python3 python3-pip
+        sudo pacman -Syu --noconfirm python python-pip
     else
         echo "Python3 is already installed."
     fi
+
+    if ! command -v pipx &>/dev/null; then
+        echo "pipx not found, installing..."
+        # Installa pipx utilizzando pip
+        python -m pip install --user pipx
+        # Aggiungi il percorso di pipx alla variabile PATH
+        export PATH="$HOME/.local/bin:$PATH"
+        # Verifica se pipx è stato installato correttamente
+        if command -v pipx &>/dev/null; then
+            echo "pipx successfully installed."
+        else
+            echo "Failed to install pipx."
+        fi
+    else
+        echo "pipx is already installed."
+    fi
 }
+
+# Chiamata alla funzione di installazione
+install_python_and_pipx
