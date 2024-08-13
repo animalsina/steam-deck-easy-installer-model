701e3c940d20a49c128323995bfa92cb4ed890c8
AnimaLSina <peppenps@gmail.com>
2024-08-13 13:10:18 +0200

    Run upgrade

--- bin/run.sh
+++ bin/run.sh
@@ -1,26 +1,72 @@
 #!/bin/bash
 
-# Import functions from external files
-source install_python.sh
-source install_flatpak.sh
-source install_python_libraries.sh
-source install_flatpak_app.sh
-source configure_steam_and_images.sh
+# URL of the Git repository
+GIT_REPO_URL="https://github.com/animalsina/steam-deck-easy-installer-model.git"
 
-# Check that an argument has been passed to the script
-if [ -z "$1" ]; then
-    echo "Usage: $0 <flatpak-app-name>"
-    exit 1
-fi
-
-# Flatpak app name passed as an argument
+# Name of the Flatpak application (passed as an argument)
 APP_NAME=$1
 
-# Run the installations and configurations
-install_python
-install_flatpak
-install_python_libraries
-install_flatpak_app $APP_NAME
-configure_steam_and_images $APP_NAME
+# Name of the directory where the repository will be cloned
+REPO_DIR="temp_{$APP_NAME}_git_repo"
 
-echo "Installation and configuration completed."
+
+# Function to check if git is installed
+install_git_if_needed() {
+    if ! command -v git &> /dev/null; then
+        echo "Git is not installed. Installing..."
+        sudo apt update
+        sudo apt install -y git
+    else
+        echo "Git is already installed."
+    fi
+}
+
+# Function to clone the Git repository
+clone_git_repo() {
+    if [ -d "$REPO_DIR" ]; then
+        rm -rf "$REPO_DIR"
+    fi
+    git clone "$GIT_REPO_URL" "$REPO_DIR"
+}
+
+# Function to make the scripts executable
+make_scripts_executable() {
+    chmod +x "$REPO_DIR/scripts/bash/install_python.sh"
+    chmod +x "$REPO_DIR/scripts/bash/install_python_libraries.sh"
+    chmod +x "$REPO_DIR/scripts/bash/install_flatpak_app.sh"
+    chmod +x "$REPO_DIR/scripts/bash/configure_steam_and_images.sh"
+}
+
+# Main function to run the installation and configuration
+run_installation() {
+    source "$REPO_DIR/scripts/bash/install_python.sh"
+    source "$REPO_DIR/scripts/bash/install_python_libraries.sh"
+    source "$REPO_DIR/scripts/bash/install_flatpak_app.sh"
+    source "$REPO_DIR/scripts/bash/configure_steam_and_images.sh"
+
+    if [ -z "$APP_NAME" ]; then
+        echo "Usage: $0 <flatpak-app-name>"
+        exit 1
+    fi
+
+    install_python
+    install_python_libraries
+    install_flatpak_app "$APP_NAME"
+    configure_steam_and_images "$APP_NAME"
+
+    echo "Installation and configuration completed."
+}
+
+# Function to remove the Git repository directory
+cleanup() {
+    rm -rf "$REPO_DIR"
+    echo "Git repository directory removed."
+    echo "Operation completed successfully."
+}
+
+# Run the script
+install_git_if_needed
+clone_git_repo
+make_scripts_executable
+run_installation
+cleanup
